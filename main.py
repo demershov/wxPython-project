eb3e90f451a455676bd97559f52dd93eabb6e0d3
Dmitry Ershov <degalweb@icloud.com>
2019-03-21 05:17:11 +0300

    Created editing function songs tags.

--- main.py
+++ main.py
@@ -5,6 +5,45 @@ import eyed3
 
 class EditDialog(wx.Dialog):
     def __init__(self, mp3):
+        title = f'Редактирование {mp3.tag.title}'
+        super().__init__(parent=None, title=title)
+        self.mp3 = mp3
+        self.main_sizer = wx.BoxSizer(wx.VERTICAL)
+
+        self.title = wx.TextCtrl(
+            self, value=self.mp3.tag.title
+        )
+        self.add_widgets('Название', self.title)
+
+        self.artist = wx.TextCtrl(self, value=self.mp3.tag.artist)
+        self.add_widgets('Артист', self.artist)
+
+        self.album = wx.TextCtrl(self, value=self.mp3.tag.album)
+        self.add_widgets('Альбом', self.album)
+
+        btn_sizer = wx.BoxSizer()
+        save_btn = wx.Button(self, label='Сохранить')
+        save_btn.Bind(wx.EVT_BUTTON, self.on_save)
+
+        btn_sizer.Add(save_btn, 0, wx.ALL, 5)
+        btn_sizer.Add(wx.Button(
+            self, label='Отменить', id=wx.ID_CANCEL), 0, wx.ALL, 5)
+        self.main_sizer.Add(btn_sizer, 0, wx.CENTER)
+        self.SetSizer(self.main_sizer)
+
+    def add_widgets(self, label_text, text_ctrl):
+        row_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        label = wx.StaticText(self, label=label_text, size=(50, -1))
+        row_sizer.Add(label, 0, wx.ALL, 5)
+        row_sizer.Add(text_ctrl, 1, wx.ALL | wx.EXPAND, 5)
+        self.main_sizer.Add(row_sizer, 0, wx.EXPAND)
+
+    def on_save(self, event):
+        self.mp3.tag.artist = self.artist.GetValue()
+        self.mp3.tag.album = self.album.GetValue()
+        self.mp3.tag.title = self.title.GetValue()
+        self.mp3.tag.save()
+        self.Close()
 
 
 class Mp3Panel(wx.Panel):
@@ -42,8 +81,8 @@ class Mp3Panel(wx.Panel):
         self.list_ctrl.InsertColumn(0, 'Название', width=200)
         self.list_ctrl.InsertColumn(1, 'Артист', width=140)
         self.list_ctrl.InsertColumn(2, 'Альбом', width=140)
+        mp3s = glob.glob(folder_path + '/*.mp3')
 
-        mp3s = glob.glob(f'{folder_path}/*.mp3)')
         mp3_objects = []
         for index, mp3 in enumerate(mp3s):
             mp3_object = eyed3.load(mp3)
